5ee3da406561b221e86bdcb5a9f1d4be2d34bef8
abhishek yadav <ay6392915727@gmail.com>
2026-04-05 12:03:15 +0530

    core fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://real-time-chat-application-rjnf.vercel.app",
     credentials: true,
   })
 );
