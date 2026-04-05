5445d2b69c3a531d16580b8c322415a7f44f6b4f
abhishek yadav <ay6392915727@gmail.com>
2026-04-05 12:20:53 +0530

    fix url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance  = axios.create({
-    baseURL: import.meta.env.MODE ==="development" ? "http://localhost:5001" : "https://real-time-chat-application-jxy1.onrender.com",
+    baseURL: import.meta.env.MODE ==="development" ? "http://localhost:5001" : "https://real-time-chat-application-jxy1.onrender.com/api",
     withCredentials:true,
 })
